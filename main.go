8cbbe75f1cc770ea1b0b1c36ae30e1a749f023f2
Matt Everett <matt.everett@mediakind.com>
2023-09-30 00:43:32 +0100

    Print the output as JSON

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"sharefinder/model"
 )
 
+// ensureHolding returns an existing holding or creates a new one to return as required
 func ensureHolding(name string, isFund bool, holdings map[string]*model.Holding) *model.Holding {
 	holding, found := holdings[name]
 	if !found {
@@ -23,6 +24,7 @@ func ensureHolding(name string, isFund bool, holdings map[string]*model.Holding)
 	return holding
 }
 
+// processRecords takes a flat list of fund records and converts them into
 func processRecords(fundRecords []api.FundRecord) map[string]*model.Holding {
 	// Create a map of all the holdings by name
 	holdings := map[string]*model.Holding{}
@@ -61,6 +63,7 @@ func getShares(primaryHoldingName string, holdings map[string]*model.Holding) ([
 }
 
 func main() {
+	// Read from a fixed file
 	jsonFile, err := os.Open("data/example.json")
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -68,21 +71,22 @@ func main() {
 	}
 	defer jsonFile.Close()
 
+	// Read the file data into memory
 	var fundRecords []api.FundRecord
 	bytes, _ := io.ReadAll(jsonFile)
 	json.Unmarshal(bytes, &fundRecords)
 
-	fmt.Println(fundRecords)
-
+	// Structure the data so we can traverse the relationships
 	holdings := processRecords(fundRecords)
-	fmt.Println()
-	fmt.Printf("%v\n", holdings)
 
+	// Now we can recurse through the holdings and find the shares
 	shares, err := getShares("Ethical Global Fund", holdings)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
 
-	fmt.Println()
-	fmt.Printf("%v\n", shares)
+	// Print the result to stdout in json format
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", "    ")
+	encoder.Encode(shares)
 }
